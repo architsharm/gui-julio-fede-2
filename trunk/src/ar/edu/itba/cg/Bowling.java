f29c4a45bcde9cfd8145c9b5463e38d638fe4ca8
fmaste <fmaste@f102e826-98e8-11de-b5b0-958946df26fc>
2009-10-29 23:49:21 +0000

    CANALETAS!!!! Hago commit antes de cagarla!

--- trunk/src/ar/edu/itba/cg/Bowling.java
+++ trunk/src/ar/edu/itba/cg/Bowling.java
@@ -8,6 +8,7 @@ import com.jme.bounding.BoundingBox;
 import com.jme.light.PointLight;
 import com.jme.math.Matrix3f;
 import com.jme.math.Plane;
+import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
 import com.jme.renderer.ColorRGBA;
 import com.jme.scene.Node;
@@ -35,9 +36,11 @@ import com.jme.scene.shape.Sphere;
 public class Bowling extends SimpleGame {
 	private static final String IMAGE_LOGO = "resources/logo.jpg";
 	private static final String TITLE = "Bowling";
-	private static final int BALL_RADIUS = 20;
+	private static final float BALL_RADIUS = 21.8F;
+	private static final float BALL_WEIGHT = 7255;
+	private static final int BALL_SAMPLES = 50;
 	private static final float GUTTER_EXTRA = 1.10F;
-	private static final int GUTTER_SAMPLES = 20;
+	private static final int GUTTER_SAMPLES = 100;
 	// The middle of the foul line is at 0, BALL_RADIUS, 0
 	// Ten Pin Bowling: http://en.wikipedia.org/wiki/Tenpin
 	private static final int LANE_WIDTH = 105; // centimeters
@@ -57,7 +60,9 @@ public class Bowling extends SimpleGame {
 	
 	@Override
 	protected void simpleInitGame() {
+		float BALL_DIAMETER = BALL_RADIUS * 2;
 		float BALL_RADIUS_EXTRA = BALL_RADIUS * GUTTER_EXTRA;
+		float BALL_DIAMETER_EXTRA = BALL_RADIUS_EXTRA * 2;
 		// Title
 		display.setTitle(TITLE);
 		// Backgournd color
@@ -73,12 +78,12 @@ public class Bowling extends SimpleGame {
 		lane.updateModelBound();
 		rootNode.attachChild( lane );
 		// Approach
-		Box approach = new Box( "approach", new Vector3f(-(LANE_WIDTH/2 + BALL_RADIUS_EXTRA), BALL_RADIUS_EXTRA, 0), new Vector3f(LANE_WIDTH/2 + BALL_RADIUS_EXTRA, 0, APPROACH_LENGTH) );
+		Box approach = new Box( "approach", new Vector3f(-(LANE_WIDTH/2 + BALL_DIAMETER_EXTRA), BALL_RADIUS_EXTRA, 0), new Vector3f(LANE_WIDTH/2 + BALL_DIAMETER_EXTRA, 0, APPROACH_LENGTH) );
 		approach.setModelBound( new BoundingBox() ); 
 		approach.updateModelBound();
 		rootNode.attachChild( approach );
 		// Ball
-		Sphere ball = new Sphere("ball", new Vector3f(0, BALL_RADIUS * 2, 0), 50, 50, BALL_RADIUS);
+		Sphere ball = new Sphere("ball", new Vector3f(0, BALL_DIAMETER, 0), BALL_SAMPLES, BALL_SAMPLES, BALL_RADIUS);
 		ball.setModelBound( new BoundingBox() ); 
 		ball.updateModelBound();
 		ball.setDefaultColor( ColorRGBA.red.clone() );
@@ -88,17 +93,20 @@ public class Bowling extends SimpleGame {
 		float circumference = 2.0F * (float)Math.PI * BALL_RADIUS_EXTRA;
 		Node gutterLeft = new Node("gutter_left");
 		Node gutterRight = new Node("gutter_left");
-		for( int i = 0; i < GUTTER_SAMPLES; i++ ) {
-			Quad left = new Quad( "gutter_left_" + String.valueOf(i),  circumference / GUTTER_SAMPLES, LANE_LENGTH);
-			Quad right = new Quad( "gutter_right_" + String.valueOf(i), circumference / GUTTER_SAMPLES , LANE_LENGTH);
-			//left.setLocalRotation( new Matrix3f() );
-			left.setLocalTranslation(0, 0, -(LANE_LENGTH / 2) );
+		for( int i = 1; i < GUTTER_SAMPLES; i++ ) {
+			Quad left =  new Quad( "gutter_left_"  + String.valueOf(i), circumference / GUTTER_SAMPLES, LANE_LENGTH);
+			Quad right = new Quad( "gutter_right_" + String.valueOf(i), circumference / GUTTER_SAMPLES, LANE_LENGTH);
+			float rotation =  -(float)Math.PI/2 + (float)Math.PI / GUTTER_SAMPLES * i; 
+			left.setLocalRotation(  new Quaternion( new float[]{ (float)Math.PI/2, 0, rotation } ) );
+			right.setLocalRotation( new Quaternion( new float[]{ (float)Math.PI/2, 0, rotation } ) );
+			float angle = (float)Math.PI + (float)Math.PI / GUTTER_SAMPLES * i;
+			left.setLocalTranslation( -(LANE_WIDTH/2 + BALL_RADIUS_EXTRA) + BALL_RADIUS_EXTRA * (float)Math.cos( angle ), BALL_RADIUS_EXTRA + BALL_RADIUS_EXTRA * (float)Math.sin( angle ), -(LANE_LENGTH / 2) );
+			right.setLocalTranslation( LANE_WIDTH/2 + BALL_RADIUS_EXTRA + BALL_RADIUS_EXTRA * (float)Math.cos( angle ), BALL_RADIUS_EXTRA + BALL_RADIUS_EXTRA * (float)Math.sin( angle ), -(LANE_LENGTH / 2) );
 			gutterLeft.attachChild( left );
 			gutterLeft.attachChild( right );
 		}
 		rootNode.attachChild( gutterLeft );
 		rootNode.attachChild( gutterRight );
-		
 		// Light state
 		lightState = display.getRenderer().createLightState();
         lightState.detachAll();
