7d832373fd6117a027be8633c097129c4580ca6e
fmaste <fmaste@f102e826-98e8-11de-b5b0-958946df26fc>
2009-12-08 23:25:06 +0000

    Listo las camaras, es un quilombo hacerlas bien con chase camera

--- trunk/src/ar/edu/itba/cg/CameraManager.java
+++ trunk/src/ar/edu/itba/cg/CameraManager.java
@@ -11,13 +11,15 @@ public class CameraManager {
 	private Dynamics dynamics;
 	private InputHandler input;
 	private InputHandler actualInput;
+	private SceneParameters params;
 	
 	
-	public CameraManager( Scene scene, Dynamics dynamics, InputHandler input, InputHandler actualInput ) {
+	public CameraManager( Scene scene, Dynamics dynamics, InputHandler input, InputHandler actualInput, SceneParameters params ) {
 		this.scene = scene;
 		this.dynamics = dynamics;
 		this.input = input;
 		this.actualInput = actualInput;
+		this.params = params;
 	}
 	
 
@@ -31,13 +33,17 @@ public class CameraManager {
 	public void setBallCamera() {
 		Camera cam = DisplaySystem.getDisplaySystem().getRenderer().getCamera();
 		InputHandler cameraInputHandler = new ChaseCamera( cam, dynamics.ball );
-		cameraInputHandler.setActionSpeed( 0.3F );
+		cameraInputHandler.setActionSpeed( 5.3F );
 		((ChaseCamera)cameraInputHandler).setDampingK( 0 );
 		((ChaseCamera)cameraInputHandler).setSpringK( 0 );
         ((ChaseCamera)cameraInputHandler).setMaxDistance( 3 );
         ((ChaseCamera)cameraInputHandler).setMinDistance( 2 );
-        ((ChaseCamera)cameraInputHandler).setTargetOffset( new Vector3f(0,0.4F,1) );
+        ((ChaseCamera)cameraInputHandler).setTargetOffset( new Vector3f( 0, params.BALL_DIAMETER, -params.BOX_LENGTH ) );
 		changeInput( cameraInputHandler );
+//		cam.setUp( new Vector3f(0,1,0) );
+//		cam.setLeft( new Vector3f(-1,0,0) );
+//		cam.setDirection( new Vector3f(0,0,-1) );
+//		cam.setLocation( new Vector3f( dynamics.ball.getLocalTranslation().x, dynamics.ball.getLocalTranslation().y + params.BALL_RADIUS, dynamics.ball.getLocalTranslation().z + params.BOX_LENGTH) );
 	}
 	
 	
@@ -46,7 +52,11 @@ public class CameraManager {
 		InputHandler cameraInputHandler = new ChaseCamera( cam, dynamics.anchor );
         ((ChaseCamera)cameraInputHandler).setMaxDistance( 2 );
         ((ChaseCamera)cameraInputHandler).setMinDistance( 1 );
-		changeInput( cameraInputHandler );		
+		changeInput( cameraInputHandler );
+//		cam.setUp( new Vector3f(0,1,0) );
+//		cam.setLeft( new Vector3f(-1,0,0) );
+//		cam.setDirection( new Vector3f(0,0,-1) );
+//		cam.setLocation( new Vector3f(0,params.BALL_DIAMETER_EXTRA,0) );
 	}
 	
 	
